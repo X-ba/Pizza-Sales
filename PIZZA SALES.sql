ccdb4ea193fb9e867414f3adbce6c8e77ce738ff
Kgomotso Xaba <xabakgomotso3@gmail.com>
2025-05-03 13:03:27 +0200

    Add files via upload
    In this project I perform exploration techniques with MySQL server to assess the Sales performance in 2015.

---
+++ PIZZA SALES.sql
@@ -0,0 +1,62 @@
+use pizza_s;
+select * from pizza_sales;
+
+#Total Revenue:
+SELECT SUM(total_price) AS Total_Revenue FROM pizza_sales;
+
+#Average Order Value
+SELECT (SUM(total_price) / COUNT(DISTINCT order_id)) AS Avg_order_Value FROM pizza_sales;
+
+#Total Pizzas Sold
+SELECT SUM(quantity) AS Total_pizza_sold FROM pizza_sales;
+
+#Total Orders
+SELECT COUNT(DISTINCT order_id) AS Total_Orders FROM pizza_sales;
+
+#Average Pizzas Per Order
+SELECT CAST(CAST(SUM(quantity) AS DECIMAL(10,2)) / 
+CAST(COUNT(DISTINCT order_id) AS DECIMAL(10,2)) AS DECIMAL(10,2))
+AS Avg_Pizzas_per_order
+FROM pizza_sales;
+
+#of Sales by Pizza Category
+SELECT pizza_category, CAST(SUM(total_price) AS DECIMAL(10,2)) as total_revenue,
+CAST(SUM(total_price) * 100 / (SELECT SUM(total_price) from pizza_sales) AS DECIMAL(10,2)) AS PCT
+FROM pizza_sales
+GROUP BY pizza_category;
+
+
+#Hourly Trend for Orders
+SELECT HOUR(order_time) AS order_hours, COUNT(DISTINCT order_id) AS total_hourly_orders
+FROM pizza_sales
+GROUP BY HOUR(order_time)
+ORDER BY HOUR(order_time);
+
+
+#% of Sales by Pizza Size
+SELECT pizza_size, CAST(SUM(total_price) AS DECIMAL(10,2)) as total_revenue,
+CAST(SUM(total_price) * 100 / (SELECT SUM(total_price) from pizza_sales) AS DECIMAL(10,2)) AS PCT
+FROM pizza_sales
+GROUP BY pizza_size
+ORDER BY pizza_size;
+
+#Top 5 Best Sellers by Total Pizzas Sold
+SELECT pizza_name, SUM(quantity) AS Total_Pizza_Sold
+FROM pizza_sales
+GROUP BY pizza_name
+ORDER BY Total_Pizza_Sold DESC
+limit 5;
+
+#Top 5 Least Sold by Total Pizzas Sold
+SELECT pizza_name, SUM(quantity) AS Total_Pizza_Sold
+FROM pizza_sales
+GROUP BY pizza_name
+ORDER BY Total_Pizza_Sold ASC
+limit 5;
+
+#Total Pizzas Sold by Pizza Category
+SELECT pizza_category, SUM(quantity) as Total_Quantity_Sold
+FROM pizza_sales
+WHERE MONTH(order_date) = 2
+GROUP BY pizza_category
+ORDER BY Total_Quantity_Sold DESC;
